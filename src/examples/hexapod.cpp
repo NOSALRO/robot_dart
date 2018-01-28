d48bd86a1fd1881a89bfc42b881f9167e8a52e7c
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2018-01-28 22:02:25 +0100

    Add dummy HexaControl for ease of use between projects

--- src/examples/hexapod.cpp
+++ src/examples/hexapod.cpp
@@ -1,43 +1,11 @@
 #include <iostream>
 #include <robot_dart/robot_dart_simu.hpp>
 
-#include <robot_dart/control/policy_control.hpp>
-
-#include <hexapod_controller/hexapod_controller_simple.hpp>
+#include <robot_dart/control/hexa_control.hpp>
 
 #ifdef GRAPHIC
 #include <robot_dart/graphics/graphics.hpp>
 #endif
-
-using namespace hexapod_controller;
-
-struct HexaPolicy {
-    void set_params(const std::vector<double>& ctrl)
-    {
-        _controller.set_parameters(ctrl);
-    }
-
-    size_t output_size() const { return 18; }
-
-    Eigen::VectorXd query(const std::shared_ptr<robot_dart::Robot>& robot, double t)
-    {
-        auto angles = _controller.pos(t);
-
-        Eigen::VectorXd target_positions = Eigen::VectorXd::Zero(18 + 6);
-        for (size_t i = 0; i < angles.size(); i++)
-            target_positions(i + 6) = ((i % 3 == 1) ? 1.0 : -1.0) * angles[i];
-
-        Eigen::VectorXd q = robot->skeleton()->getPositions();
-        Eigen::VectorXd q_err = target_positions - q;
-
-        double gain = 1.0 / (dart::math::constants<double>::pi() * robot->skeleton()->getTimeStep());
-        Eigen::VectorXd vel = q_err * gain;
-
-        return vel.tail(18);
-    }
-
-    HexapodControllerSimple _controller;
-};
 
 int main()
 {
@@ -55,8 +23,7 @@ int main()
     // g_robot->add_controller(std::make_shared<robot_dart::SimpleControl>());
     std::vector<double> ctrl = {1, 0, 0.5, 0.25, 0.25, 0.5, 1, 0.5, 0.5, 0.25, 0.75, 0.5, 1, 0, 0.5, 0.25, 0.25, 0.5, 1, 0, 0.5, 0.25, 0.75, 0.5, 1, 0.5, 0.5, 0.25, 0.25, 0.5, 1, 0, 0.5, 0.25, 0.75, 0.5};
 
-    using HexaControl = robot_dart::control::PolicyControl<HexaPolicy>;
-    g_robot->add_controller(std::make_shared<HexaControl>(ctrl));
+    g_robot->add_controller(std::make_shared<robot_dart::control::HexaControl>(ctrl));
 
     robot_dart::RobotDARTSimu simu;
 #ifdef GRAPHIC
