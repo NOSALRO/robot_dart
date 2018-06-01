478f40861be7ba48bf89dde24713015f475cc092
jspitz <jonathan.spitz@inria.fr>
2018-06-01 12:58:05 +0000

    Improved the simulation time check to make sure it doesn't keep going due to an insignificant time difference.

--- src/robot_dart/robot_dart_simu.cpp
+++ src/robot_dart/robot_dart_simu.cpp
@@ -6,6 +6,8 @@
 #include <dart/dynamics/BoxShape.hpp>
 #include <dart/dynamics/WeldJoint.hpp>
 
+#include <iostream>
+
 namespace robot_dart {
     RobotDARTSimu::RobotDARTSimu(double time_step) : _world(std::make_shared<dart::simulation::World>()),
                                                      _old_index(0),
@@ -28,8 +30,9 @@ namespace robot_dart {
         _break = false;
         size_t index = _old_index;
         double old_t = _world->getTime();
+	double factor = _world->getTimeStep() / 2.;
 
-        while ((_world->getTime() - old_t) < max_duration && !_graphics->done()) {
+        while ((_world->getTime() - old_t - max_duration) < -factor && !_graphics->done()) {
             for (auto& robot : _robots)
                 robot->update(_world->getTime());
 
