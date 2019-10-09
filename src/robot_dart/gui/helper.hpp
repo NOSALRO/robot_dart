15762e19727759a2c1aff515effb0bc04ee6df35
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2019-10-09 10:25:10 +0200

    Magnum: added grayscale conversion

--- src/robot_dart/gui/helper.hpp
+++ src/robot_dart/gui/helper.hpp
@@ -7,7 +7,12 @@
 namespace robot_dart {
     namespace gui {
         using Image = std::vector<std::vector<std::vector<uint8_t>>>;
+        using GrayscaleImage = std::vector<std::vector<uint8_t>>;
+
         void save_png_image(const std::string& filename, const Image& rgb);
+        void save_png_image(const std::string& filename, const GrayscaleImage& gray);
+
+        GrayscaleImage convert_rgb_to_grayscale(const Image& rgb);
     } // namespace gui
 } // namespace robot_dart
 
