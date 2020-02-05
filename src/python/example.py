337f86076a76e0817b36c8460fd6b2d0ecfc2ced
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2020-02-05 16:46:47 +0100

    Fix in cameras examples + [python]: add parallel graphics example

--- src/python/example.py
+++ src/python/example.py
@@ -3,6 +3,7 @@ import dartpy
 import RobotDART as rd
 # import magnum
 
+# Create custom controller
 class MyController(rd.RobotControl):
     def __init__(self, ctrl, full_control):
         rd.RobotControl.__init__(self, ctrl, full_control)
@@ -15,37 +16,47 @@ class MyController(rd.RobotControl):
         cmd = 100*(target-self.get_positions())
         return cmd[0]
 
+    # TO-DO: This is NOT working at the moment!
+    def clone(self):
+        return MyController(self._ctrl, self._full_control)
+
+# Load robot from URDF
 robot = rd.Robot("res/models/arm.urdf", "arm", False)
 robot.fix_to_world()
 
-control = MyController([0.0, 1.0, -1.5, 1.0], False)
-
-# pdcontrol = rd.PDControl([0.0, 1.0, -1.5, 1.0], False)
-# robot.add_controller(pdcontrol, 1.)
-# pdcontrol.set_pd(200., 20.)
+# Initiate custom controller
+control = MyController([0.0, 2, -0.5, 0.7], False)
+# Add it to the robot
 robot.add_controller(control, 1.)
 
+# Print initial positions of the robot
 print(control.get_positions())
 
+# Create simulator object
 simu = rd.RobotDARTSimu(0.001)
-simu.add_robot(robot)
 
+# Create graphics
 graphics = rd.gui.Graphics(simu.world(), 640, 480, True, False, "DART")
 # graphics.clear_lights()
 # mat = rd.gui.Material(magnum.Color4(0, 0, 0, 1), magnum.Color4(1, 1, 1, 1), magnum.Color4(1, 1, 1, 1), 80.)
 # graphics.add_light(rd.gui.createPointLight(magnum.Vector3(-1., 1., 2.), mat, 2., magnum.Vector3(0., 0., 1.)))
 # graphics.add_light(rd.gui.createPointLight(magnum.Vector3(1., -1., 2.), mat, 2., magnum.Vector3(0., 0., 1.)))
 simu.set_graphics(graphics)
+
+# Add robot and floor to the simulation
+simu.add_robot(robot)
 simu.add_checkerboard_floor(10., 0.1, 1., np.zeros((6,1)), "floor")
 
+# Add a camera to the end-effector of the manipulator
 camera = rd.gui.CameraOSR(simu.world(), graphics.magnum_app(), 256, 256)
-camera.attach_to("arm_link_5", dartpy.math.Isometry3())
+rot = dartpy.math.AngleAxis(3.14, [1., 0., 0.]).to_rotation_matrix()
+rot = rot.dot(dartpy.math.AngleAxis(1.57, [0., 0., 1.]).to_rotation_matrix())
+camera.attach_to("arm_link_5", dartpy.math.Isometry3(rotation=rot, translation=[0., 0., 0.1]))
 simu.add_camera(camera)
-# simu.add_camera(rd.gui.CameraOSR(simu.world(), graphics.magnum_app(), 256, 256))
 
 simu.run(5.)
 
-# img = simu.camera(0).image()
-# rd.gui.save_png_image('test.png', img)
+img = camera.image()
+rd.gui.save_png_image('camera.png', img)
 
 print(control.get_positions())
