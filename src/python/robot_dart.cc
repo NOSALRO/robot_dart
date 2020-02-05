8daa091f041fa02f17c04adc968a8e349f600269
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2020-02-05 15:37:25 +0100

    [python]: make it possible to run parallel graphics

--- src/python/robot_dart.cc
+++ src/python/robot_dart.cc
@@ -1,4 +1,5 @@
 #include <pybind11/eigen.h>
+#include <pybind11/functional.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
@@ -354,8 +355,15 @@ void py_gui(py::module& m)
 
         .def("magnum_app", &WindowlessGraphics::magnum_app, py::return_value_policy::reference);
 
-    // get_gl_context
-    // release_gl_context
+    sm.def("run_with_gl_context", +[](const std::function<void()>& func, size_t wait_ms) {
+        get_gl_context_with_sleep(my_context, wait_ms);
+        /* Acquire GIL before calling Python code */
+        py::gil_scoped_acquire acquire;
+        func();
+        py::gil_scoped_release release;
+        release_gl_context(my_context);
+    });
+
     sm.def("set_max_contexts", +[](size_t num_contexts) {
         gui::magnum::GlobalData::instance()->set_max_contexts(num_contexts);
     });
