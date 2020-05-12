12a3537ae554c1881e69cb5cd62155c92cf62251
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2020-05-12 19:21:26 +0200

    last issue is with GLFW on osx

---
+++ cmake/example/CMakeFiles/robot_dart_example_graphics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robot_dart_example_graphics.dir/example.cpp.o"
+  "robot_dart_example_graphics"
+  "robot_dart_example_graphics.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot_dart_example_graphics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake/example/CMakeFiles/robot_dart_example_graphics.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for robot_dart_example_graphics.
+# This may be replaced when dependencies are built.
