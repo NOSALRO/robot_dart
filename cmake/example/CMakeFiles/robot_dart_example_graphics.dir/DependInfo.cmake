04442382d35cc650a8f2714f0c152f70ef12b3f5
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2020-05-12 19:21:26 +0200

    last issue is with GLFW on osx

---
+++ cmake/example/CMakeFiles/robot_dart_example_graphics.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jmouret/git/resibots/robot_dart/cmake/example/example.cpp" "/Users/jmouret/git/resibots/robot_dart/cmake/example/CMakeFiles/robot_dart_example_graphics.dir/example.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_DATE_TIME_DYN_LINK"
+  "BOOST_FILESYSTEM_DYN_LINK"
+  "BOOST_IOSTREAMS_DYN_LINK"
+  "BOOST_SYSTEM_DYN_LINK"
+  "BOOST_TEST_DYN_LINK"
+  "BOOST_THREAD_DYN_LINK"
+  "BUILD_DEPRECATED"
+  "BUILD_MULTITHREADED"
+  "GRAPHIC"
+  "MAGNUM_MAC_OSX"
+  "MAGNUM_PLUGINS_AUDIOIMPORTER_DIR=\"/usr/local/lib/magnum/audioimporters\""
+  "MAGNUM_PLUGINS_DIR=\"/usr/local/lib/magnum\""
+  "MAGNUM_PLUGINS_FONTCONVERTER_DIR=\"/usr/local/lib/magnum/fontconverters\""
+  "MAGNUM_PLUGINS_FONT_DIR=\"/usr/local/lib/magnum/fonts\""
+  "MAGNUM_PLUGINS_IMAGECONVERTER_DIR=\"/usr/local/lib/magnum/imageconverters\""
+  "MAGNUM_PLUGINS_IMPORTER_DIR=\"/usr/local/lib/magnum/importers\""
+  "TARGET_GL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/jmouret/include"
+  "/usr/local/include/eigen3"
+  "/usr/local/include"
+  "/usr/local/Cellar/fcl/0.6.1/include"
+  "/usr/local/Cellar/assimp/4.1.0/include"
+  "/usr/local/Cellar/tinyxml2/7.1.0/include"
+  "/usr/local/Cellar/urdfdom/1.0.3/include"
+  "/usr/local/Cellar/urdfdom_headers/1.0.4/include"
+  "/usr/local/include/bullet"
+  "/usr/local/Cellar/ode/0.16/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
