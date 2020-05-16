289d873f231483818e1ad3faf1abdc71da531170
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2020-05-16 20:21:48 +0300

    [ci]: trying a different yml

--- ci/install_dart_latest.sh
+++ ci/install_dart_latest.sh
@@ -1,5 +1,8 @@
 sudo apt-add-repository ppa:dartsim/ppa -y
 sudo apt-get -qq update
+
+sudo apt-get -qq --yes --force-yes install libccd-dev libfcl-dev
+
 git clone git://github.com/dartsim/dart.git
 cd dart
 mkdir build
@@ -7,11 +10,11 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j4
 sudo make install
-cd ..
-mkdir build_py
-cd build_py
-cmake -DDART_BUILD_DARTPY=ON -DCMAKE_BUILD_TYPE=Release ..
-make -j4
-sudo make install
+# cd ..
+# mkdir build_py
+# cd build_py
+# cmake -DDART_BUILD_DARTPY=ON -DCMAKE_BUILD_TYPE=Release ..
+# make -j4
+# sudo make install
 sudo ldconfig
 cd $CI_HOME
