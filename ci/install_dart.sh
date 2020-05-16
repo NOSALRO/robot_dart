146817c0f2c83a479d11017425332a101ff97b88
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2020-05-16 20:35:48 +0300

    [ci]: trying a different yml

--- ci/install_dart.sh
+++ ci/install_dart.sh
@@ -1,8 +1,10 @@
-sudo apt-add-repository ppa:dartsim/ppa -y
+sudo apt-get -qq update
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository -y ppa:dartsim/ppa
 sudo apt-get -qq update
 
-sudo apt-get -qq --yes --force-yes install libccd-dev libfcl-dev
+sudo apt-get -qq --yes --force-yes install libassimp-dev libccd-dev libfcl-dev
 
 sudo apt-get -qq --yes --force-yes install libdart6-dev libdart6-utils-urdf-dev #python3-dartpy
 
-if [ "$DART_MASTER" = "OFF" && "$DART_EXTRA" = "ON" ]; then 'sudo apt-get -qq --yes --force=yes install libdart-collision-ode-dev libdart-collision-bullet-dev' ; fi
+if [ "$DART_MASTER" = "OFF" ] && [ "$DART_EXTRA" = "ON" ]; then 'sudo apt-get -qq --yes --force=yes install libdart-collision-ode-dev libdart-collision-bullet-dev' ; fi
