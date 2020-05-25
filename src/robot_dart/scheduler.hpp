fa28e68eb921ac54baac31ae498342d55b728cab
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2020-05-25 16:41:00 +0200

    remove example from hpp

--- src/robot_dart/scheduler.hpp
+++ src/robot_dart/scheduler.hpp
@@ -49,27 +49,3 @@ protected:
     using clock_t = std::chrono::high_resolution_clock;
     clock_t::time_point _start;
 };
-
-#if 0 // example
-int main()
-{
-    Scheduler scheduler(1e-3, true);
-    while (scheduler.next_time() < 5) {
-
-        if (scheduler(1000)) {
-            std::cout << 1000 << std::endl;
-        }
-
-        if (scheduler(50)) {
-           std::cout << 50 << std::endl;
-        }
-
-        if (scheduler(20)) {
-           std::cout << 20 << std::endl;
-        }
-
-        scheduler.sync();
-    }
-    return 0;
-}
-#endif
