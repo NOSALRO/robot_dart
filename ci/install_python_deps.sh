5ce68bf1639052fbcbb0492aca0c83d73ce7a5a6
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2020-05-29 17:36:46 +0300

    [ci]: fix for pythonpath?

--- ci/install_python_deps.sh
+++ ci/install_python_deps.sh
@@ -33,4 +33,7 @@ else
     HOMEBREW_NO_AUTO_UPDATE=1 brew install pybind11
 
     sudo -H python3 -m pip install --user numpy
+    ls /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages/
+    ls /usr/local/lib/python3.7/site-packages/
+    export PYTHONPATH="${PYTHONPATH}:/usr/local/lib/python3.7/site-packages/"
 fi
