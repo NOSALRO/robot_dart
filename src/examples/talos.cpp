567403e7b8b2f71fcb2ea47495b8465b0726923e
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2020-06-04 22:16:14 +0200

    start adding the talos (not working yet)

---
+++ src/examples/talos.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <robot_dart/control/pd_control.hpp>
+#include <robot_dart/robot_dart_simu.hpp>
+
+#include <dart/collision/fcl/FCLCollisionDetector.hpp>
+#include <dart/constraint/ConstraintSolver.hpp>
+
+#ifdef GRAPHIC
+#include <robot_dart/gui/magnum/graphics.hpp>
+#endif
+
+int main()
+{
+    std::srand(std::time(NULL));
+
+    std::vector<std::pair<std::string, std::string>> packages = {{"talos_description", "res/robots/talos/talos_description"}};
+    auto global_robot = std::make_shared<robot_dart::Robot>("res/robots/talos/talos.urdf", packages);
+
+    global_robot->set_position_enforced(true);
+    global_robot->skeleton()->setPosition(5, 1.2);
+    global_robot->skeleton()->setPosition(2, 1.57);
+
+    // Set actuator types to VELOCITY motors so that they stay in position without any controller
+    global_robot->set_actuator_types(dart::dynamics::Joint::VELOCITY);
+    // First 6-DOFs should always be FORCE if robot is floating base
+    for (size_t i = 0; i < 6; i++)
+        global_robot->set_actuator_type(i, dart::dynamics::Joint::FORCE);
+
+    robot_dart::RobotDARTSimu simu(0.001);
+    simu.world()->getConstraintSolver()->setCollisionDetector(dart::collision::FCLCollisionDetector::create());
+#ifdef GRAPHIC
+    auto graphics = std::make_shared<robot_dart::gui::magnum::Graphics<>>(&simu);
+    simu.set_graphics(graphics);
+    graphics->look_at({0., 3.5, 2.}, {0., 0., 0.25});
+#endif
+    simu.add_checkerboard_floor();
+    simu.add_robot(global_robot);
+    simu.run(20.);
+
+    global_robot.reset();
+    return 0;
+}
