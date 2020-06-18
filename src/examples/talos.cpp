e3982c6b695180e0ecf6987e934c4ea628fe872d
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2020-06-18 13:18:30 +0300

    [talos]: replace black with dark gray, better example

--- src/examples/talos.cpp
+++ src/examples/talos.cpp
@@ -23,10 +23,11 @@ int main()
     robot->skeleton()->setPosition(5, 1.1);
     robot->skeleton()->setPosition(2, 1.57);
 
-    // Set actuator types to VELOCITY motors so that they stay in position without any controller
+    // Set actuator types to VELOCITY (for speed)
     robot->set_actuator_types("velocity");
 
-    robot_dart::RobotDARTSimu simu(0.001);
+    double dt = 0.001;
+    robot_dart::RobotDARTSimu simu(dt);
     simu.set_collision_detector("fcl");
 #ifdef GRAPHIC
     auto graphics = std::make_shared<robot_dart::gui::magnum::Graphics>(&simu);
@@ -44,15 +45,18 @@ int main()
         "arm_right_2_joint",
         "torso_1_joint"};
 
+    Eigen::VectorXd init_positions = robot->positions(dofs);
+
     auto start = std::chrono::steady_clock::now();
-    while (simu.scheduler().next_time() < 20 && !simu.graphics()->done()) {
+    while (simu.scheduler().next_time() < 20. && !simu.graphics()->done()) {
         if (simu.schedule(simu.control_freq())) {
-            Eigen::VectorXd commands(5);
-            commands << sin(simu.scheduler().current_time() * 4),
-                sin(simu.scheduler().current_time() * 4),
-                sin(simu.scheduler().current_time() * 4),
-                sin(simu.scheduler().current_time() * 4),
-                sin(simu.scheduler().current_time() * 4);
+            Eigen::VectorXd delta_pos(5);
+            delta_pos << sin(simu.scheduler().current_time() * 2.),
+                sin(simu.scheduler().current_time() * 2.),
+                sin(simu.scheduler().current_time() * 2.),
+                sin(simu.scheduler().current_time() * 2.),
+                sin(simu.scheduler().current_time() * 2.);
+            Eigen::VectorXd commands = (init_positions + delta_pos) - robot->positions(dofs);
             robot->set_commands(commands, dofs);
         }
         simu.step_world();
