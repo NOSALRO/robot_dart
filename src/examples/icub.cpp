1732de7b29992f4098475a88acfc35ce7658f5cb
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2020-10-02 14:50:18 +0200

    add icub

--- src/examples/icub.cpp
+++ src/examples/icub.cpp
@@ -9,6 +9,7 @@
 
 #include <robot_dart/sensor/force_torque.hpp>
 #include <robot_dart/sensor/imu.hpp>
+#include <robot_dart/robots/icub.hpp>
 
 #ifdef GRAPHIC
 #include <robot_dart/gui/magnum/graphics.hpp>
@@ -18,22 +19,15 @@ int main()
 {
     std::srand(std::time(NULL));
 
-    std::vector<std::pair<std::string, std::string>> packages = {{"icub_description", "icub/icub_description"}};
-    auto robot = std::make_shared<robot_dart::Robot>("icub/icub.urdf", packages);
+    robot_dart::RobotDARTSimu simu(0.001);
+    simu.set_collision_detector("fcl");
 
-    robot->set_color_mode("material");
-    robot->set_color_mode("aspect", "right_foot");
-    robot->set_color_mode("aspect", "left_foot");
-
-    robot->set_position_enforced(true);
-    robot->skeleton()->setPosition(5, 0.5);
-    robot->skeleton()->setPosition(2, 1.57);
-
+    auto robot = std::make_shared<robot_dart::robots::ICub>(&simu);
     // Set actuator types to VELOCITY motors so that they stay in position without any controller
     robot->set_actuator_types("velocity");
 
-    robot_dart::RobotDARTSimu simu(0.001);
-    simu.set_collision_detector("fcl");
+
+
 #ifdef GRAPHIC
     auto graphics = std::make_shared<robot_dart::gui::magnum::Graphics>(&simu);
     simu.set_graphics(graphics);
@@ -43,18 +37,10 @@ int main()
     simu.add_checkerboard_floor();
     simu.add_robot(robot);
 
-    // Add an IMU sensor to the "chest" body link
-    robot_dart::sensor::IMUConfig imu_config;
-    imu_config.body = robot->body_node("chest"); // choose which body the sensor is attached to
-    imu_config.frequency = 200; // update rate of the sensor
-    auto imu_sensor = simu.add_sensor<robot_dart::sensor::IMU>(&simu, imu_config);
-
-    // Add a force/torque sensor in "r_ankle_roll" joint
-    auto ft_sensor = simu.add_sensor<robot_dart::sensor::ForceTorque>(&simu, robot, "r_ankle_roll");
 
     // Add some visualizations
-    robot->set_draw_axis(imu_config.body->getName());
-    robot->set_draw_axis("r_ankle_2");
+    // robot->set_draw_axis(imu_config.body->getName());
+    // robot->set_draw_axis("r_ankle_2");
 
     simu.set_control_freq(100); // 100 Hz
     std::vector<std::string> dofs = {
@@ -93,16 +79,16 @@ int main()
         simu.step_world();
 
         // Print IMU measurements
-        if (simu.schedule(imu_sensor->frequency())) {
-            std::cout << "Angular    Velocity: " << imu_sensor->angular_velocity().transpose().format(fmt) << std::endl;
-            std::cout << "Linear Acceleration: " << imu_sensor->linear_acceleration().transpose().format(fmt) << std::endl;
+        if (simu.schedule(robot->imu()->frequency())) {
+            std::cout << "Angular    Velocity: " << robot->imu()->angular_velocity().transpose().format(fmt) << std::endl;
+            std::cout << "Linear Acceleration: " << robot->imu()->linear_acceleration().transpose().format(fmt) << std::endl;
             std::cout << "=================================" << std::endl;
         }
 
         // Print FT measurements
-        if (simu.schedule(ft_sensor->frequency())) {
-            std::cout << "FT ( force): " << ft_sensor->force().transpose().format(fmt) << std::endl;
-            std::cout << "FT (torque): " << ft_sensor->torque().transpose().format(fmt) << std::endl;
+        if (simu.schedule(robot->ft_foot_left()->frequency())) {
+            std::cout << "FT ( force): " << robot->ft_foot_left()->force().transpose().format(fmt) << std::endl;
+            std::cout << "FT (torque): " << robot->ft_foot_left()->torque().transpose().format(fmt) << std::endl;
             std::cout << "=================================" << std::endl;
         }
     }
