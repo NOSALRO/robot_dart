d02f1a4a1b7e0a211d02d3302248e00a840795a3
dinies <edoardo.ghini@live.it>
2020-10-07 17:35:44 +0200

    adapted the urdf file of the franka robot and added an example of it

--- src/examples/franka.cpp
+++ src/examples/franka.cpp
@@ -1,46 +1,64 @@
-#include <algorithm>
-#include <cstdlib>
 #include <iostream>
+#include <robot_dart/robot_dart_simu.hpp>
 
 #include <robot_dart/control/pd_control.hpp>
-#include <robot_dart/robot_dart_simu.hpp>
 
 #ifdef GRAPHIC
 #include <robot_dart/gui/magnum/graphics.hpp>
 #endif
 
+
+
 int main()
 {
     std::srand(std::time(NULL));
 
-    std::vector<std::pair<std::string, std::string>> packages = {{"iiwa_description", "iiwa/iiwa_description"}};
-    auto global_robot = std::make_shared<robot_dart::Robot>("iiwa/iiwa.urdf", packages);
+    std::vector<std::pair<std::string, std::string>> packages = {{"franka_description", "franka/franka_description"}};
+    auto global_robot = std::make_shared<robot_dart::Robot>("franka/franka.urdf", packages);
 
+    // pin the arm to world
     global_robot->fix_to_world();
     global_robot->set_position_enforced(true);
 
-    Eigen::VectorXd ctrl(7);
-    ctrl << 0., M_PI / 3., 0., -M_PI / 4., 0., 0., 0.;
+    for( auto s : global_robot->actuator_types()){
 
+      std::cout << "type of actuators" << s <<"\n";
+    }
+
+
+    global_robot->set_actuator_types("velocity");
+
+
+    // add a PD-controller to the arm
+    // set desired positions
+    Eigen::VectorXd ctrl(8);
+    ctrl << 0., 0., 0., 0., 0., 0., 0.,0. ;
+
+
+    // add the controller to the robot
     global_robot->add_controller(std::make_shared<robot_dart::control::PDControl>(ctrl));
     std::static_pointer_cast<robot_dart::control::PDControl>(global_robot->controllers()[0])->set_pd(300., 50.);
 
     // Add a ghost robot; only visuals, no dynamics, no collision
     auto ghost = global_robot->clone_ghost();
 
+
+    // choose time step of 0.001 seconds
     robot_dart::RobotDARTSimu simu(0.001);
     simu.set_collision_detector("fcl");
+
 #ifdef GRAPHIC
     auto graphics = std::make_shared<robot_dart::gui::magnum::Graphics>(&simu);
     simu.set_graphics(graphics);
-
-    graphics->look_at({0., 3.5, 2.}, {0., 0., 0.25});
+    // set the camera at position (0, 3, 1) looking at the center (0, 0, 0)
+    graphics->look_at({0., 3., 1.}, {0., 0., 0.});
 #endif
+
     simu.add_checkerboard_floor();
     simu.add_robot(global_robot);
     simu.add_robot(ghost);
-    simu.run(20.);
 
+    simu.run(10.);
     global_robot.reset();
     return 0;
 }
