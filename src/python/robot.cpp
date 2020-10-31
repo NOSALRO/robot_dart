366e810c2e8fa784060eb22303488165fa4fec56
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2020-10-31 09:14:29 +0200

    [robot]: better helper functions for damping, coulomb friction and spring stifness

--- src/python/robot.cpp
+++ src/python/robot.cpp
@@ -8,6 +8,7 @@
 #include <robot_dart/control/robot_control.hpp>
 
 #include <dart/dynamics/BodyNode.hpp>
+#include <dart/dynamics/DegreeOfFreedom.hpp>
 #include <dart/dynamics/Joint.hpp>
 
 namespace robot_dart {
@@ -50,6 +51,12 @@ namespace robot_dart {
 
                 .def("joint", (dart::dynamics::Joint * (Robot::*)(size_t)) & Robot::joint, py::return_value_policy::reference,
                     py::arg("joint_index"))
+
+                .def("dof", (dart::dynamics::DegreeOfFreedom * (Robot::*)(const std::string&)) & Robot::dof, py::return_value_policy::reference,
+                    py::arg("dof_name"))
+
+                .def("dof", (dart::dynamics::DegreeOfFreedom * (Robot::*)(size_t)) & Robot::dof, py::return_value_policy::reference,
+                    py::arg("dof_index"))
 
                 .def("name", &Robot::name)
                 .def("model_filename", &Robot::model_filename)
@@ -103,26 +110,45 @@ namespace robot_dart {
                 .def("actuator_types", &Robot::actuator_types,
                     py::arg("joint_names") = std::vector<std::string>())
 
-                .def("set_position_enforced", (void (Robot::*)(size_t, bool)) & Robot::set_position_enforced)
-                .def("set_position_enforced", (void (Robot::*)(const std::vector<bool>&)) & Robot::set_position_enforced)
-                .def("set_position_enforced", (void (Robot::*)(bool)) & Robot::set_position_enforced)
+                .def("set_position_enforced", (void (Robot::*)(const std::vector<bool>&, const std::vector<std::string>&)) & Robot::set_position_enforced,
+                    py::arg("enforced"),
+                    py::arg("dof_names") = std::vector<std::string>())
+                .def("set_position_enforced", (void (Robot::*)(bool, const std::vector<std::string>&)) & Robot::set_position_enforced,
+                    py::arg("enforced"),
+                    py::arg("dof_names") = std::vector<std::string>())
 
-                .def("position_enforced", (bool (Robot::*)(size_t) const) & Robot::position_enforced)
-                .def("position_enforced", (std::vector<bool>(Robot::*)() const) & Robot::position_enforced)
+                .def("position_enforced", &Robot::position_enforced,
+                    py::arg("dof_names") = std::vector<std::string>())
 
-                .def("set_damping_coeff", &Robot::set_damping_coeff)
-                .def("set_damping_coeffs", (void (Robot::*)(const std::vector<double>&)) & Robot::set_damping_coeffs)
-                .def("set_damping_coeffs", (void (Robot::*)(double)) & Robot::set_damping_coeffs)
+                .def("set_damping_coeffs", (void (Robot::*)(const std::vector<double>&, const std::vector<std::string>&)) & Robot::set_damping_coeffs,
+                    py::arg("damps"),
+                    py::arg("dof_names") = std::vector<std::string>())
+                .def("set_damping_coeffs", (void (Robot::*)(double, const std::vector<std::string>&)) & Robot::set_damping_coeffs,
+                    py::arg("damps"),
+                    py::arg("dof_names") = std::vector<std::string>())
 
-                .def("damping_coeff", &Robot::damping_coeff)
-                .def("damping_coeffs", &Robot::damping_coeffs)
+                .def("damping_coeffs", &Robot::damping_coeffs,
+                    py::arg("dof_names") = std::vector<std::string>())
 
-                .def("set_cfriction_coeff", &Robot::set_cfriction_coeff)
-                .def("set_cfriction_coeffs", (void (Robot::*)(const std::vector<double>&)) & Robot::set_cfriction_coeffs)
-                .def("set_cfriction_coeffs", (void (Robot::*)(double)) & Robot::set_cfriction_coeffs)
+                .def("set_coulomb_coeffs", (void (Robot::*)(const std::vector<double>&, const std::vector<std::string>&)) & Robot::set_coulomb_coeffs,
+                    py::arg("cfrictions"),
+                    py::arg("dof_names") = std::vector<std::string>())
+                .def("set_coulomb_coeffs", (void (Robot::*)(double, const std::vector<std::string>&)) & Robot::set_coulomb_coeffs,
+                    py::arg("cfrictions"),
+                    py::arg("dof_names") = std::vector<std::string>())
 
-                .def("cfriction_coeff", &Robot::cfriction_coeff)
-                .def("cfriction_coeffs", &Robot::cfriction_coeffs)
+                .def("coulomb_coeffs", &Robot::coulomb_coeffs,
+                    py::arg("dof_names") = std::vector<std::string>())
+
+                .def("set_spring_stiffnesses", (void (Robot::*)(const std::vector<double>&, const std::vector<std::string>&)) & Robot::set_spring_stiffnesses,
+                    py::arg("stiffnesses"),
+                    py::arg("dof_names") = std::vector<std::string>())
+                .def("set_spring_stiffnesses", (void (Robot::*)(double, const std::vector<std::string>&)) & Robot::set_spring_stiffnesses,
+                    py::arg("stiffnesses"),
+                    py::arg("dof_names") = std::vector<std::string>())
+
+                .def("spring_stiffnesses", &Robot::spring_stiffnesses,
+                    py::arg("dof_names") = std::vector<std::string>())
 
                 .def("set_friction_dir", (void (Robot::*)(const std::string&, const Eigen::Vector3d&)) & Robot::set_friction_dir,
                     py::arg("body_name"),
