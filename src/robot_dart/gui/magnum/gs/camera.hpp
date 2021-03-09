ef96ef659addb78af9c60d2a1aced19497ec0942
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2021-03-09 10:43:44 +0200

    [gui]: camera intrinsic/extrinsic fix

--- src/robot_dart/gui/magnum/gs/camera.hpp
+++ src/robot_dart/gui/magnum/gs/camera.hpp
@@ -52,7 +52,8 @@ namespace robot_dart {
                     Magnum::Float fov() const { return static_cast<Magnum::Float>(_fov); }
                     Magnum::Int width() const { return _camera->viewport()[0]; }
                     Magnum::Int height() const { return _camera->viewport()[1]; }
-                    Magnum::Matrix3 const intrinsic_matrix();
+                    Magnum::Matrix3 intrinsic_matrix() const;
+                    Magnum::Matrix4 extrinsic_matrix() const;
 
                     Camera& look_at(const Magnum::Vector3& camera, const Magnum::Vector3& center, const Magnum::Vector3& up = Magnum::Vector3::zAxis());
 
