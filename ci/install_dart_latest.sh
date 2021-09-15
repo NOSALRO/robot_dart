9c3b4da56c708bf58a325c6a7b4435682c5f06d3
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2021-09-16 02:45:34 +0300

    [GitHub]: enable CI actions

--- ci/install_dart_latest.sh
+++ ci/install_dart_latest.sh
@@ -1,28 +1,19 @@
-if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-    sudo apt-get -qq update
-    sudo apt-get -y install software-properties-common
-    sudo apt-add-repository -y ppa:dartsim/ppa
-    sudo apt-get -qq update
-
-    sudo apt-get install -y --no-install-recommends libassimp-dev libccd-dev libfcl-dev
-else
-    HOMEBREW_NO_AUTO_UPDATE=1 brew install dartsim --only-dependencies
-fi
-
+mkdir -p ~/.deps
+cd ~/.deps
 git clone git://github.com/dartsim/dart.git
 cd dart
-git checkout v6.10.1
+git checkout $DART_TAG
 mkdir build
 cd build
-if [ "$PYTHON_TESTS" = "ON" ]; then
-cmake -DDART_BUILD_DARTPY=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr ..
+if [ "$BUILD_PYTHON" = "ON" ]; then
+cmake -DDART_BUILD_DARTPY=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=~/.dart_install ..
 else
-cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr ..
+cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=~/.dart_install ..
 fi
 make -j4
 sudo make install
-if [ "$PYTHON_TESTS" = "ON" ]; then
+if [ "$BUILD_PYTHON" = "ON" ]; then
     sudo make install-dartpy
 fi
 sudo ldconfig
-cd $CI_HOME
+cd ${{github.workspace}}
--- ci/install_magnum.sh
+++ ci/install_magnum.sh
@@ -1,51 +1,41 @@
-if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-    sudo apt-get -qq update
-    sudo apt-get install -y --no-install-recommends libglfw3-dev libglfw3 libopenal-dev
+mkdir -p ~/.deps
+cd ~/.deps
+# install Corrade
+git clone https://github.com/mosra/corrade.git
+cd corrade
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=/usr ..
+make -j
+sudo make install
+cd ../..
 
-    # install Corrade
-    git clone https://github.com/mosra/corrade.git
-    cd corrade
-    mkdir build && cd build
-    cmake -DCMAKE_BUILD_TYPE=Release ..
-    make -j
-    sudo make install
-    cd ../..
+# install Magnum
+git clone https://github.com/mosra/magnum.git
+cd magnum
+mkdir build && cd build
+# Ubuntu
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DWITH_AUDIO=ON -DWITH_DEBUGTOOLS=ON -DWITH_GL=ON -DWITH_MESHTOOLS=ON -DWITH_PRIMITIVES=ON -DWITH_SCENEGRAPH=ON -DWITH_SHADERS=ON -DWITH_TEXT=ON -DWITH_TEXTURETOOLS=ON -DWITH_TRADE=ON -DWITH_GLFWAPPLICATION=ON -DWITH_WINDOWLESSGLXAPPLICATION=ON -DWITH_OPENGLTESTER=ON -DWITH_ANYAUDIOIMPORTER=ON -DWITH_ANYIMAGECONVERTER=ON -DWITH_ANYIMAGEIMPORTER=ON -DWITH_ANYSCENEIMPORTER=ON -DWITH_MAGNUMFONT=ON -DWITH_OBJIMPORTER=ON -DWITH_TGAIMPORTER=ON -DWITH_WAVAUDIOIMPORTER=ON -DCMAKE_INSTALL_PREFIX=/usr ..
+make -j
+sudo make install
+cd ../..
 
-    # install Magnum
-    git clone https://github.com/mosra/magnum.git
-    cd magnum
-    mkdir build && cd build
-    # Ubuntu
-    cmake -DCMAKE_BUILD_TYPE=Release -DWITH_AUDIO=ON -DWITH_DEBUGTOOLS=ON -DWITH_GL=ON -DWITH_MESHTOOLS=ON -DWITH_PRIMITIVES=ON -DWITH_SCENEGRAPH=ON -DWITH_SHADERS=ON -DWITH_TEXT=ON -DWITH_TEXTURETOOLS=ON -DWITH_TRADE=ON -DWITH_GLFWAPPLICATION=ON -DWITH_WINDOWLESSGLXAPPLICATION=ON -DWITH_OPENGLTESTER=ON -DWITH_ANYAUDIOIMPORTER=ON -DWITH_ANYIMAGECONVERTER=ON -DWITH_ANYIMAGEIMPORTER=ON -DWITH_ANYSCENEIMPORTER=ON -DWITH_MAGNUMFONT=ON -DWITH_OBJIMPORTER=ON -DWITH_TGAIMPORTER=ON -DWITH_WAVAUDIOIMPORTER=ON ..
-    make -j
-    sudo make install
-    cd ../..
+# install MagnumPlugins
+git clone https://github.com/mosra/magnum-plugins.git
+cd magnum-plugins
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DWITH_ASSIMPIMPORTER=ON -DWITH_DDSIMPORTER=ON -DWITH_JPEGIMPORTER=ON -DWITH_OPENGEXIMPORTER=ON -DWITH_PNGIMPORTER=ON -DWITH_TINYGLTFIMPORTER=ON -DWITH_STBTRUETYPEFONT=ON  -DCMAKE_INSTALL_PREFIX=/usr ..
+make -j
+sudo make install
+cd ../..
 
-    # install MagnumPlugins
-    git clone https://github.com/mosra/magnum-plugins.git
-    cd magnum-plugins
-    mkdir build && cd build
-    cmake -DCMAKE_BUILD_TYPE=Release -DWITH_ASSIMPIMPORTER=ON -DWITH_DDSIMPORTER=ON -DWITH_JPEGIMPORTER=ON -DWITH_OPENGEXIMPORTER=ON -DWITH_PNGIMPORTER=ON -DWITH_TINYGLTFIMPORTER=ON -DWITH_STBTRUETYPEFONT=ON ..
-    make -j
-    sudo make install
-    cd ../..
+# install MagnumIntegration
+git clone https://github.com/mosra/magnum-integration.git
+cd magnum-integration
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DWITH_DART=ON -DWITH_EIGEN=ON  -DCMAKE_INSTALL_PREFIX=/usr ..
+make -j
+sudo make install
+cd ../..
 
-    # install MagnumIntegration
-    git clone https://github.com/mosra/magnum-integration.git
-    cd magnum-integration
-    mkdir build && cd build
-    cmake -DCMAKE_BUILD_TYPE=Release -DWITH_DART=ON -DWITH_EIGEN=ON ..
-    make -j
-    sudo make install
-    cd ../..
-
-    sudo ldconfig
-    cd $CI_HOME
-else
-    sudo xcode-select -r
-    # We need a newer version than 2019.10 for Magnum
-    HOMEBREW_NO_AUTO_UPDATE=1 brew install --HEAD mosra/magnum/corrade
-    HOMEBREW_NO_AUTO_UPDATE=1 brew install --HEAD mosra/magnum/magnum
-    HOMEBREW_NO_AUTO_UPDATE=1 brew install --HEAD mosra/magnum/magnum-plugins --with-assimp
-    HOMEBREW_NO_AUTO_UPDATE=1 brew install --HEAD mosra/magnum/magnum-integration --with-dartsim --with-eigen
-fi
+sudo ldconfig
+cd ${{github.workspace}}
