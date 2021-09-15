61a3b6064a2523f112ff47f876eebaa592ad463f
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2021-09-16 02:49:53 +0300

    [GitHub]: fix CI script

--- ci/install_packages.sh
+++ ci/install_packages.sh
@@ -2,7 +2,7 @@ sudo apt-get -qq update
 sudo apt-get -y install software-properties-common
 sudo apt-add-repository -y ppa:dartsim/ppa
 sudo apt-get -qq update
-sudo apt-get install -y --no-install-recommends build-essential cmake pkg-config git libeigen3-dev libccd-dev libfcl-dev libboost-regex-dev libboostsystem-dev libbullet-dev libode-dev liboctomap-dev libtinyxml-dev libtinyxml2-dev liburdfdom-dev liburdfdom-headers-dev python3-pip python3-numpy pybind11-dev libpython3-dev libxi-dev libxmu-dev freeglut3-dev libopenscenegraph-dev libassimp-dev
+sudo apt-get install -y --no-install-recommends build-essential cmake pkg-config git libeigen3-dev libccd-dev libfcl-dev libboost-regex-dev libboost-system-dev libbullet-dev libode-dev liboctomap-dev libtinyxml-dev libtinyxml2-dev liburdfdom-dev liburdfdom-headers-dev python3-pip python3-numpy pybind11-dev libpython3-dev libxi-dev libxmu-dev freeglut3-dev libopenscenegraph-dev libassimp-dev
 
 sudo ln -s /usr/bin/python3 /usr/bin/python
 
