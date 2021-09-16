e7526da847b23873fedcff16a07274d437fce752
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2021-09-16 10:35:09 +0300

    [GitHub]: make proper selection of compilers

--- ci/install_dart_latest.sh
+++ ci/install_dart_latest.sh
@@ -5,6 +5,14 @@ cd dart
 git checkout $DART_TAG
 mkdir build
 cd build
+# Select Compiler
+if [ "$COMPILER" = "gcc" ]; then
+export CC=/usr/bin/gcc && export CXX=/usr/bin/g++
+fi
+if [ "$COMPILER" = "clang" ]; then
+export CC=/usr/bin/clang && export CXX=/usr/bin/clang++
+fi
+# Build DART
 if [ "$BUILD_PYTHON" = "ON" ]; then
 cmake -DDART_BUILD_DARTPY=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=~/.dart_install ..
 else
