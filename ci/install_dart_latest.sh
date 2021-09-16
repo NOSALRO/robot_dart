e41e64348f0838c47266ba00dcb992c4c43ebdd6
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2021-09-16 14:55:45 +0300

    [GitHub]: it will fail

--- ci/install_dart_latest.sh
+++ ci/install_dart_latest.sh
@@ -18,9 +18,9 @@ export CC=/usr/bin/clang && export CXX=/usr/bin/clang++
 fi
 # Build DART
 if [ "$BUILD_PYTHON" = "ON" ]; then
-cmake -DDART_BUILD_DARTPY=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=~/.dart_install ..
+cmake -DDART_BUILD_DARTPY=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=/home/runner/.dart_install ..
 else
-cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=~/.dart_install ..
+cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=/home/runner/.dart_install ..
 fi
 make -j4
 sudo make install
