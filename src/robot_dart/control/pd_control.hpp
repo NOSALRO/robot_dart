ce7bf5728994985bdab3cef276a0553784479cb1
dinies <edoardo.ghini@live.it>
2021-09-27 10:05:22 +0000

    added constructor flag angular erros, fixed formatting, moved impl of func to cpp, cleaned import in hpp, changed name of func

--- src/robot_dart/control/pd_control.hpp
+++ src/robot_dart/control/pd_control.hpp
@@ -7,16 +7,14 @@
 #include <robot_dart/control/robot_control.hpp>
 #include <robot_dart/robot.hpp>
 
-#include <dart/dynamics/RevoluteJoint.hpp>
-
 namespace robot_dart {
     namespace control {
 
         class PDControl : public RobotControl {
         public:
             PDControl();
-            PDControl(const Eigen::VectorXd& ctrl, bool full_control = false);
-            PDControl(const Eigen::VectorXd& ctrl, const std::vector<std::string>& controllable_dofs);
+            PDControl(const Eigen::VectorXd& ctrl, bool full_control = false, const bool angular_errors = true);
+            PDControl(const Eigen::VectorXd& ctrl, const std::vector<std::string>& controllable_dofs, const bool angular_errors = true);
 
             void configure() override;
             Eigen::VectorXd calculate(double) override;
@@ -31,18 +29,10 @@ namespace robot_dart {
         protected:
             Eigen::VectorXd _Kp;
             Eigen::VectorXd _Kd;
+            bool _angular_errors;
 
-            inline double angle_dist(const double alfa, const double beta){
-              double theta = alfa - beta;
-              while (theta < -M_PI)
-                theta += 2 * M_PI;
-              while (theta > M_PI)
-                theta -= 2 * M_PI;
-              return theta;
-            }
-
+            double _angle_dist(const double alfa, const double beta);
         };
     } // namespace control
 } // namespace robot_dart
-
 #endif
