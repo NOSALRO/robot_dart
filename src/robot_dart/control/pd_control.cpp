16e1a551da4449054ada07d09fdc3485b46c8b3d
dinies <edoardo.ghini@live.it>
2021-09-27 10:05:22 +0000

    added constructor flag angular erros, fixed formatting, moved impl of func to cpp, cleaned import in hpp, changed name of func

--- src/robot_dart/control/pd_control.cpp
+++ src/robot_dart/control/pd_control.cpp
@@ -1,14 +1,13 @@
 #include "pd_control.hpp"
 #include "robot_dart/robot.hpp"
 #include "robot_dart/utils.hpp"
+#include <dart/dynamics/RevoluteJoint.hpp>
 
 namespace robot_dart {
     namespace control {
         PDControl::PDControl() : RobotControl() {}
-        PDControl::PDControl(const Eigen::VectorXd& ctrl, bool full_control) : RobotControl(ctrl, full_control) {}
-        PDControl::PDControl(const Eigen::VectorXd& ctrl, const std::vector<std::string>& controllable_dofs) : RobotControl(ctrl, controllable_dofs) {}
-
-
+        PDControl::PDControl(const Eigen::VectorXd& ctrl, bool full_control, const bool angular_errors) : RobotControl(ctrl, full_control), _angular_errors(angular_errors) {}
+        PDControl::PDControl(const Eigen::VectorXd& ctrl, const std::vector<std::string>& controllable_dofs, const bool angular_errors) : RobotControl(ctrl, controllable_dofs), _angular_errors(angular_errors) {}
 
         void PDControl::configure()
         {
@@ -28,20 +27,20 @@ namespace robot_dart {
             Eigen::VectorXd q = robot->positions(_controllable_dofs);
             Eigen::VectorXd dq = robot->velocities(_controllable_dofs);
 
-
-            Eigen::VectorXd error= Eigen::VectorXd::Zero(_control_dof);
-            for(int i=0;i<_control_dof;++i){
-              if(robot->dof(_controllable_dofs[i])->getJoint()->getType() ==
-                  dart::dynamics::RevoluteJoint::getStaticType()){
-                error(i)= angle_dist( target_positions(i), q(i));
-              }
-              else{
-                error(i)= target_positions(i)- q(i);
-              }
+            Eigen::VectorXd error = Eigen::VectorXd::Zero(_control_dof);
+            for (int i = 0; i < _control_dof; ++i) {
+                if (robot->dof(_controllable_dofs[i])->getJoint()->getType() == dart::dynamics::RevoluteJoint::getStaticType()
+                    and _angular_errors) {
+                    error(i) = _angle_dist(target_positions(i), q(i));
+                }
+                else {
+                    error(i) = target_positions(i) - q(i);
+                }
             }
 
             /// Compute the simplest PD controller output:
-            /// P gain * (target position - current position) + D gain * (0 - current velocity)
+            /// P gain * (target position - current position) + D gain * (0 -
+            /// current velocity)
             Eigen::VectorXd commands = _Kp.array() * error.array() - _Kd.array() * dq.array();
 
             return commands;
@@ -69,6 +68,16 @@ namespace robot_dart {
         std::shared_ptr<RobotControl> PDControl::clone() const
         {
             return std::make_shared<PDControl>(*this);
+        }
+
+        double PDControl::_angle_dist(const double alfa, const double beta)
+        {
+            double theta = alfa - beta;
+            while (theta < -M_PI)
+                theta += 2 * M_PI;
+            while (theta > M_PI)
+                theta -= 2 * M_PI;
+            return theta;
         }
     } // namespace control
 } // namespace robot_dart
