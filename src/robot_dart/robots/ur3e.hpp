d6ab50bfcd704cd98ce6f25fb8b39ca06a47a3c8
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2022-01-20 22:02:23 +0100

    support ur3e + Schunk hand

---
+++ src/robot_dart/robots/ur3e.hpp
@@ -0,0 +1,22 @@
+#ifndef ROBOT_DART_ROBOTS_FRANKA_HPP
+#define ROBOT_DART_ROBOTS_FRANKA_HPP
+
+#include "robot_dart/robot.hpp"
+#include "robot_dart/sensor/force_torque.hpp"
+
+namespace robot_dart {
+    namespace robots {
+        class Ur3e : public Robot {
+        public:
+            Ur3e(size_t frequency = 1000, const std::string& urdf = "ur3e/ur3e_with_schunk_hand.urdf", const std::vector<std::pair<std::string, std::string>>& packages = {{"ur3e_description", "ur3e/ur3e_description"}});
+
+            const sensor::ForceTorque& ft_wrist() const { return *_ft_wrist; }
+
+        protected:
+            std::shared_ptr<sensor::ForceTorque> _ft_wrist;
+            void _post_addition(RobotDARTSimu* simu) override;
+            void _post_removal(RobotDARTSimu* simu) override;
+        };
+    } // namespace robots
+} // namespace robot_dart
+#endif
